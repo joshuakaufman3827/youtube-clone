594eb9d3b475a15eeb5fe821354d2a71f13f3a5e
joshuakaufman3827 <148819008+joshuakaufman3827@users.noreply.github.com>
2026-07-28 12:06:02 -0500

    Initial commit

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import './Navbar.css'
-import menu_icon from 'youtube-clone/src/assets/menu.png'
-import logo from 'youtube-clone/src/assets/logo.png'
-import search_icon from 'youtube-clone/src/assets/search.png'
-import upload_icon from 'youtube-clone/src/assess/upload.png.'
-import more_icon from 'youtube-clone/src/assets/more.png'
-import notification_icon from 'youtube-clone/src/assets/notification.png'
-import profile_icon from 'youtube-clone/src/assets/jack.png'
+import menu_icon from "../../assets/menu.png";
+import logo from "../../assets/logo.png";
+import search_icon from "../../assets/search.png";
+import upload_icon from "../../assets/upload.png";
+import more_icon from "../../assets/more.png";
+import notification_icon from "../../assets/notification.png";
+import profile_icon from "../../assets/jack.png";
 
 
 
